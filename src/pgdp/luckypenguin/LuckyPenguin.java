2a430862947acff54009f8ec7938f8e0981b4f4e
Nikolai Khalatiani <65181600+NikolaiKhalatiani@users.noreply.github.com>
2022-06-05 23:19:15 +0400

    Update LuckyPenguin.java

--- src/pgdp/luckypenguin/LuckyPenguin.java
+++ src/pgdp/luckypenguin/LuckyPenguin.java
@@ -4,100 +4,94 @@ import static pgdp.MiniJava.*;
 
 public class LuckyPenguin {
     public static void main(String[] args) {
-        int num = readInt("Number of penguins:");
-        while (num <= 1)
-            num = readInt("Number of penguins should be >1:");
-        int numfish = readInt("Starting fish per penguin:");
-        while (numfish <= 0)
-            numfish = readInt("Starting fish should be >0:");
-        int[] pingu = new int[num];
-        for (int i = 0; i < num; i++) {
-            pingu[i] = numfish;
+        int numberOfPenguins = readInt("Number of penguins:");
+        while (numberOfPenguins <= 1)
+            numberOfPenguins = readInt("Number of penguins should be >1:");
+        int numFish = readInt("Starting fish per penguin:");
+        while (numFish <= 0)
+            numFish = readInt("Starting fish should be >0:");
+        int[] numOfFishEachPenguinHas = new int[numberOfPenguins];
+        for (int i = 0; i < numberOfPenguins; i++) {
+            numOfFishEachPenguinHas[i] = numFish;
         }
-        boolean test = false;
-        int[] field = new int[13];
-        int[] check = new int[num];
-        int k = 0;
-        int firstdice;
-        int seconddice;
+        int[] playField = new int[13];
+        int[] bankruptPenguinsArray = new int[numberOfPenguins];
+        int playerNum = 0;
+        int firstDice;
+        int secondDice;
         while (true) {
-            if (check[k] == 1) {
-                k++;
-                if (k == num ) k=0;
+            if (bankruptPenguinsArray[playerNum] == 1) {
+                playerNum++;
+                if (playerNum == numberOfPenguins) playerNum = 0;
                 continue;
             }
-            int sum;
-            writeBoard(field[3], field[4], field[5], field[6], field[7], field[8], field[9], field[10], field[11]);
-            write("It's penguin " + k + " turn:");
-            int stop = 0;
-            for (int i = 0; i < num; i++) {
-                if (check[i] == 1) stop++;
+            writeBoard(playField[3], playField[4], playField[5], playField[6], playField[7], playField[8], playField[9], playField[10], playField[11]);
+            System.out.println("It's penguin " + playerNum + " turn:");
+            int quittersCounter = 0;
+            for (int i = 0; i < numberOfPenguins; i++) {
+                if (bankruptPenguinsArray[i] == 1) quittersCounter++;
             }
-            if (stop == num - 1) {
-                write("You are the last penguin to play! You win all the fish on the board!");
+            if (quittersCounter == numberOfPenguins - 1) {
+                System.out.println("You are the last penguin to play! You win all the fish on the board!");
                 for (int i = 1; i < 10; i++) {
-                    pingu[k] += field[i];
+                    numOfFishEachPenguinHas[playerNum] += playField[i];
                 }
-                test = true;
                 break;
             }
-            firstdice = dice();
-            seconddice = dice();
-            sum = firstdice + seconddice;
-            write(firstdice + " + " + seconddice + " = " + sum + " was rolled.");
+            int sum;
+            firstDice = dice();
+            secondDice = dice();
+            sum = firstDice + secondDice;
+            System.out.println(firstDice + " + " + secondDice + " = " + sum + " was rolled.");
             if (sum == 12) {
-                write("King Penguin! You win all the fish on the board!");
-                for (int i = 1; i < field.length -1; i++) {
-                    pingu[k] += field[i];
-                    field[i] = 0;
+                System.out.println("King Penguin! You win all the fish on the board!");
+                for (int i = 1; i < playField.length - 1; i++) {
+                    numOfFishEachPenguinHas[playerNum] += playField[i];
+                    playField[i] = 0;
                 }
             } else if (sum == 7) {
-                write("Wedding! You give a fish and place it on F7.");
-                field[7]++;
-                pingu[k]--;
+                System.out.println("Wedding! You give a fish and place it on F7.");
+                playField[7]++;
+                numOfFishEachPenguinHas[playerNum]--;
             } else if (sum == 2) {
-                write("Lucky penguin! You win all fish on the board except F7!");
-                for (int i = 1; i < field.length - 1; i++) {
+                System.out.println("Lucky penguin! You win all fish on the board except F7!");
+                for (int i = 1; i < playField.length - 1; i++) {
                     if (i == 5) continue;
-                    pingu[k] += field[i];
-                    field[i] = 0;
+                    numOfFishEachPenguinHas[playerNum] += playField[i];
+                    playField[i] = 0;
                 }
-            } else if (field[sum] == 0) {
-                write("You put a fish on F" + sum + ".");
-                pingu[k]--;
-                field[sum]++;
+            } else if (playField[sum] == 0) {
+                System.out.println("You put a fish on F" + sum + ".");
+                numOfFishEachPenguinHas[playerNum]--;
+                playField[sum]++;
             } else {
-                write("They take the fish from F" + sum + ".");
-                pingu[k]++;
-                field[sum]--;
+                System.out.println("They take the fish from F" + sum + ".");
+                numOfFishEachPenguinHas[playerNum]++;
+                playField[sum]--;
             }
-            if (pingu[k] == 1) write("You now have " + pingu[k] + " fish!");
-            else write("You now have " + pingu[k] + " fishes!");
-            if (pingu[k] == 0) {
-                write("You have lost all fish, so you can no longer play!");
-                check[k] = 1;
+            System.out.println("You now have " + numOfFishEachPenguinHas[playerNum] + " fish!");
+            if (numOfFishEachPenguinHas[playerNum] == 0) {
+                System.out.println("You have lost all fish, so you can no longer play!");
+                bankruptPenguinsArray[playerNum] = 1;
             } else {
                 int ask = readInt("Enter 1 to exit now:");
                 if (ask == 1) {
-                    check[k] = 1;
+                    bankruptPenguinsArray[playerNum] = 1;
                 }
             }
-            k++;
-            if (k == num) k = 0;
+            playerNum++;
+            if (playerNum == numberOfPenguins) playerNum = 0;
         }
-        int WinningNum = pingu[0];
-        for (int i = 0; i < num; i++) {
-            if (pingu[i] > WinningNum) {
-                WinningNum = pingu[i];
+        int WinningNum = numOfFishEachPenguinHas[0];
+        for (int i = 0; i < numberOfPenguins; i++) {
+            if (numOfFishEachPenguinHas[i] > WinningNum) {
+                WinningNum = numOfFishEachPenguinHas[i];
             }
         }
-        if (WinningNum == 1) write("The winning penguins with " + WinningNum + " fish:");
-        else {
-            write("The winning penguins with " + WinningNum + " fishes:");
-        }
-        for (int i = 0; i < num; i++) {
-            if (pingu[i] == WinningNum) {
-                write("Penguin " + i);
+        System.out.println("The winning penguins with " + WinningNum + " fish:");
+        for (int i = 0; i < numberOfPenguins; i++) {
+            if (numOfFishEachPenguinHas[i] == WinningNum) {
+                System.out.println("Penguin " + i);
             }
         }
     }
